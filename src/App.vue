071f1df6b2981fdf5f0782b815ebad043754204f
soobinnee <oh990625@gmail.com>
2026-07-15 09:05:38 +0900

    [feat/layout] : 앱 기본 레이아웃 구성

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <AppHeader />
+
+    <main class="app-content">
+      <router-view />
+    </main>
+
+    <ChatbotWidget />
+  </div>
+</template>
+
+<script setup>
+import AppHeader from './components/layout/AppHeader.vue'
+import ChatbotWidget from './components/layout/ChatbotWidget.vue'
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.app-content {
+  flex: 1;
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
