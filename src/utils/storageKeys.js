4429baaa52145616207965bd3752a56b858801f9
Jinhyo <dlwlsgy111@gmail.com>
2026-07-15 14:00:28 +0900

    [notice/rename]:저장 한번더

---
+++ src/utils/storageKeys.js
@@ -0,0 +1,11 @@
+export const STORAGE_KEYS = {
+  POSTS: 'localhub_posts',
+  PLACES_LITE: 'localhub_places_lite',
+  CHAT_HISTORY: 'localhub_chat_history',
+  USER_LIKES: 'localhub_user_likes',
+  DASHBOARD_STATS: 'localhub_dashboard_stats',
+  POST_DRAFT: 'localhub_post_draft',
+  COMMENTS: 'localhub_comments'
+}
+
+export default STORAGE_KEYS
